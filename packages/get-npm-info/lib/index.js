28b8d3c05f4d0f0dec09af5028237ba77b8e4769
569835014 <569835014@qq.com>
2021-02-14 01:25:09 +0800

    初始化

--- packages/get-npm-info/lib/index.js
+++ packages/get-npm-info/lib/index.js
@@ -54,6 +54,7 @@ function getDefaultRegistry( isOriginal = true ) {
 async function getNpmLatestVersion( npmName, registry ) {
     let versions = await getNpmVersion(npmName, registry);
     if (versions) {
+
         const newVersions = versions.sort((( a, b ) => semver.gt(b, a) ? 1 : -1))
         log.verbose(npmName, newVersions)
         return newVersions[0];
